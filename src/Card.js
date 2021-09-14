255797126a86f9f0538ca80843334bfa3f5e754f
Agneus <76863188+agneus@users.noreply.github.com>
2021-09-14 19:58:44 -0400

    Add files via upload

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Card({anime}) {
-    console.log(anime);
+    //console.log(anime);
     return (
         <article class='card'>
             <a href={anime.url} target="_blank" rel="noreferer">
@@ -11,7 +11,8 @@ function Card({anime}) {
                 <h3 class='red'>{anime.title}</h3>
                 <h3 class='blue'>MAL Score: {anime.score}</h3>
                 <h3 class='blue'>Episodes: {anime.episodes}</h3>
-                <h3 class='blue'>Rated: {anime.rated}</h3>
+                {(anime.rank) ? (<h3 class='blue'>Rank: {anime.rank}</h3>) : ''}
+                {(anime.rated) ? (<h3 class='blue'>Rated: {anime.rated}</h3>) : ''}
                 <h3 class='blue'>MAL Fandom Size: {anime.members}</h3>    
             </a>
         </article>
